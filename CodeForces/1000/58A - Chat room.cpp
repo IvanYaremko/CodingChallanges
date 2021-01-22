b5920e4189b5d5b7af46fba4409451c6e3d7de65
Ivan <ivan.yaremko@gmail.com>
2021-01-22 21:37:15 +0000

    Update 58A - Chat room.cpp

--- CodeForces/1000/58A - Chat room.cpp
+++ CodeForces/1000/58A - Chat room.cpp
@@ -5,14 +5,12 @@ int main(){
   int hIndex=0, c=0;
 	string s, h="hello";
 	cin>>s;
-			
 	for(unsigned i=0; i<s.length(); i++){
 		if(s[i] == h[hIndex]){
 			hIndex++;
 			c++;
 			if(c==5) break;
 		}
-		
 	}
 	(c==5) ? cout<<"YES" : cout<<"NO";
 }
